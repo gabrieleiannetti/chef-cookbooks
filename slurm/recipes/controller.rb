a18e81efcd6b3b7219d99848f74f5d9ed775a1c0
Gabriele Iannetti <g.iannetti@gsi.de>
2016-08-16 16:00:44 +0200

    updated slurm config

--- slurm/recipes/controller.rb
+++ slurm/recipes/controller.rb
@@ -10,21 +10,33 @@ cookbook_file '/etc/slurm-llnl/slurm.conf' do
 end
 
 
+directory '/var/lib/slurm-llnl/state_checkpoint' do
+  owner 'slurm'
+  group 'slurm'
+  mode '0755'
+  action :create
+end
+
+
+directory '/var/lib/slurm-llnl/job_checkpoint' do                                
+  owner 'slurm'                                                                    
+  group 'slurm'                                                                    
+  mode '0755'                                                                      
+  action :create                                                                   
+end     
+
+
 service 'slurmctld' do                                                           
   action [:enable, :start]                                                    
 end
 
 
-# TODO MAKE THIS IDEMPOTENT -> MAYBE CALLING "sacctmgr list cluster" in a custom LWRP?
-
-# TODO: If Cluster already exists the return code 1 is returned. How to handle that otherwise? Wrap that call into a ressource?
 execute('Adding Slurm Cluster') do                                             
     command "sacctmgr add cluster snowflake -i"                                
     returns [0,1]                                                              
 end
 
 
-# TODO: If nothing to do then the return code 1 is returned.                    
 execute('Adding Slurm Account') do                                             
     command "sacctmgr -i add account slurm Cluster=snowflake Description='none' Organization='none'"
     returns [0,1]                                                              
